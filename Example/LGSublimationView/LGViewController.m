97ec30a42376d0d081e5af320fc27e0b6c740914
Luke Geiger <lukejamesgeiger@gmail.com>
2015-05-12 20:29:00 -0400

    Updated Demo

--- Example/LGSublimationView/LGViewController.m
+++ Example/LGSublimationView/LGViewController.m
@@ -25,7 +25,7 @@
     LGSublimationView *lgSublimer = [[LGSublimationView alloc]initWithFrame:self.view.bounds];
     
     NSMutableArray*views = [NSMutableArray new];
-    for (int i  = 0; i<4; i++) {
+    for (int i  = 1; i<=4; i++) {
         UIImageView *view = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
         view.image = [UIImage imageNamed:[NSString stringWithFormat:@"%i.jpg",i+1]];
         [views addObject:view];
