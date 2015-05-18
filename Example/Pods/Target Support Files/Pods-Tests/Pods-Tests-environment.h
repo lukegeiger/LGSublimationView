7961f3b0eb13f18410b0456e4794f1528e66c7f3
Luke Geiger <lukejamesgeiger@gmail.com>
2015-05-18 15:43:19 -0400

    Updated Pods

--- Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -9,6 +9,6 @@
 // LGSublimationView
 #define COCOAPODS_POD_AVAILABLE_LGSublimationView
 #define COCOAPODS_VERSION_MAJOR_LGSublimationView 0
-#define COCOAPODS_VERSION_MINOR_LGSublimationView 2
+#define COCOAPODS_VERSION_MINOR_LGSublimationView 3
 #define COCOAPODS_VERSION_PATCH_LGSublimationView 0
 
